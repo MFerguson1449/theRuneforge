6f5221cad5724206b83b25c671543f4b1bb539f4
MFerguson1449 <mferguson1449@gmail.com>
2021-02-06 21:42:34 -0500

    styling updates

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('it works!');
+require('dotenv').config();
+const blizzard = require('blizzard.js');
+const wowId = process.env.WOW_ID; // WoW Client ID
+const wowSecret = process.env.WOW_SECRET; // WoW Client Secret
+
+// const wow = await blizzard.wow.createInstance({
+//   key: wowId,
+//   secret: wowSecret,
+//   origin: 'us', 
+//   locale: 'en_US'
+// });
